d2a63a2ee96bd3b7f764a438e5686f4dd7d5487e
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-23 10:04:13 +0300

    build 1

---
+++ CISSP/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 254, green: 252, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 104, green: 110, blue: 163)
+    static let mainColorAlpha = UIColor(integralRed: 104, green: 110, blue: 163, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 225, green: 146, blue: 86)
+    
+    static let progress1Color = UIColor(integralRed: 104, green: 110, blue: 163)
+    static let progress2Color = UIColor(integralRed: 225, green: 146, blue: 86)
+    static let progress3Color = UIColor(integralRed: 37, green: 50, blue: 89)
+}
