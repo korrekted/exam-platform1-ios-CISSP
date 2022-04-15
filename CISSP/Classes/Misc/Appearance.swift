e1e142515ea5a2f98123edf137fee75762ea569a
akonst17 <akonst17@gmail.com>
2022-04-15 11:07:15 +0300

    upgrade; build 6

--- CISSP/Classes/Misc/Appearance.swift
+++ CISSP/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 104, green: 110, blue: 163)
     static let progress2Color = UIColor(integralRed: 225, green: 146, blue: 86)
     static let progress3Color = UIColor(integralRed: 37, green: 50, blue: 89)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
