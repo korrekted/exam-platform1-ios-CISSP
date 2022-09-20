b4e093b17441743a34a62890786609f28ec64fbd
akonst17 <akonst17@gmail.com>
2022-09-20 14:51:37 +0300

    интеграция sdk 2.0; новые урлы; build 10

--- CISSP/Classes/Misc/GlobalDefinitions.swift
+++ CISSP/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "dE5V8choEsHoVyEN"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "GZCov0GqQ2YTlJMQ"
 
     static let applicationTag = "EXAM_CISSP"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "GZCov0GqQ2YTlJMQ"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "3lIkd7vouegjFUc5"
 
     static let appStoreUrl = "https://apps.apple.com/app/cissp-exam-certification-prep/id1572742576"
     static let appleAppID = "1572742576"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
